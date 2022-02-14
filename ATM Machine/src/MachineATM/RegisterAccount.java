d6b866f3bf7bc59d03a8a652d731282d14d21244
KingNelX.v2 <88817315+KingNelx@users.noreply.github.com>
2022-02-15 00:23:50 +0800

    Update RegisterAccount.java

--- ATM Machine/src/MachineATM/RegisterAccount.java
+++ ATM Machine/src/MachineATM/RegisterAccount.java
@@ -1,7 +1,10 @@
 package MachineATM;
 
 
+import javax.management.RuntimeErrorException;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Scanner;
 
 public class RegisterAccount {
     private String firstName;
@@ -14,6 +17,9 @@ public class RegisterAccount {
         this.lastName = lastName;
         this.userID = userID;
         this.pinID = pinID;
+
+        userData(firstName, lastName, userID, pinID);
+
     }
 
 
@@ -48,5 +54,38 @@ public class RegisterAccount {
     public BigInteger getPinID(){
         return pinID;
     }
+
+    public void userData(String firstN, String lastN, BigInteger userID, BigInteger pinID){
+        Scanner gatherData = new Scanner(System.in);
+
+        System.out.print(" Enter your Firstname: ");
+        firstN = gatherData.nextLine();
+
+        System.out.print(" Enter your Lastname: ");
+        lastN = gatherData.nextLine();
+
+        System.out.print(" Enter your userID: ");
+        userID = gatherData.nextBigInteger();
+
+        System.out.print(" Enter your pinID: ");
+        pinID = gatherData.nextBigInteger();
+
+        System.out.println();
+
+        System.out.println(" Data Printing ... ");
+        System.out.println(" Firstname: " + firstN);
+        System.out.println(" Lastname: " + lastN);
+        System.out.println(" UserID: " + userID);
+        System.out.println(" PinID: " + pinID);
+
+        try {
+           if(firstN.length() < 5){
+           throw new Exception();
+           }
+        }catch (Exception e){
+            System.out.println(" You need to refill up again Maam/Sir" + firstN + " " + lastN);
+        }
+    }
+
 
 }
