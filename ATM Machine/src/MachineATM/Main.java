e6f54915bac87b8ca1ff8bd8891f56c9cb687b0a
KingNelX.v2 <88817315+KingNelx@users.noreply.github.com>
2022-02-16 00:40:29 +0800

    Update Main.java

--- ATM Machine/src/MachineATM/Main.java
+++ ATM Machine/src/MachineATM/Main.java
@@ -2,17 +2,35 @@ package MachineATM;
 
 import java.math.BigInteger;
 import java.util.Scanner;
+
 public class Main {
+    static int options;
+    static int balance;
+    static int withdraw;
+    static int deposit;
+    static String transferPerson;
+    
     public static void main (String[]args){
         Scanner input = new Scanner(System.in);
         String userFirstName = "";
         String userLastName = "";
         BigInteger userID = BigInteger.valueOf(0);
         BigInteger pinID = BigInteger.valueOf(0);
-
+        
+        
         var select = new Options();
         var userRegister = new RegisterAccount(userFirstName, userLastName, userID, pinID);
         select.selectOptions();
         select.Atm_Options();
+        options = input.nextInt();
+        
+        switch (options){
+            case 1:
+                System.out.println();
+                break;
+                
+            default:
+                System.out.println(" ");
+        }
     }
 }
