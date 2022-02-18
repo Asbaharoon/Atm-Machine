d027c9b6932434d5fda1c1875338cc41d9866780
KingNelxV2 <88817315+KingNelx@users.noreply.github.com>
2022-02-18 14:57:44 +0800

    Update RandomBalance.java

--- ATM Machine/src/MachineATM/RandomBalance.java
+++ ATM Machine/src/MachineATM/RandomBalance.java
@@ -1,13 +1,16 @@
 package MachineATM;
-import java.util.Arrays;
 import java.util.Random;
 
 public class RandomBalance {
-        public void randomBalance(){
-            long [] userBalance = {10000, 20000, 30000, 40000, 50000,
-            60000, 70000, 80000, 90000};
+        public String randomBalance(){
             Random randomBalance = new Random();
-            long balanceRandom = randomBalance.nextLong(userBalance.length);
-            System.out.println(Arrays.toString(userBalance));
+            String [] userBalance = {"10000", "20000", "30000", "40000", "50000",
+            "60000", "70000", "80000", "90000"};
+            int balanceRandom = 0;
+            balanceRandom = randomBalance.nextInt(userBalance.length);
+                System.out.println(" Random Balance has been Generated ... ");
+                System.out.println(" Balance: $" + userBalance[balanceRandom]);
+            return userBalance[balanceRandom];
+
         }
 }
