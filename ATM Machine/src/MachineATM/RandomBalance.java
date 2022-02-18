2698ecc3af2942feabe5daabd72c8592fdd5ed6f
KingNelxV2 <88817315+KingNelx@users.noreply.github.com>
2022-02-18 21:36:38 +0800

    Update RandomBalance.java

--- ATM Machine/src/MachineATM/RandomBalance.java
+++ ATM Machine/src/MachineATM/RandomBalance.java
@@ -5,7 +5,7 @@ public class RandomBalance {
         public String randomBalance(){
             Random randomBalance = new Random();
             String [] userBalance = {"10000", "20000", "30000", "40000", "50000",
-            "60000", "70000", "80000", "90000"};
+            "60000", "70000", "80000", "90000", "100000"};
             int balanceRandom = 0;
             balanceRandom = randomBalance.nextInt(userBalance.length);
                 System.out.println(" Random Balance has been Generated ... ");
